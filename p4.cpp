10049c40e322760feec2503fa742bf5d41b06a85
Michael Beale <mtbeale@hotmail.com>
2017-05-02 19:49:58 -0400

    Update p4.cpp

--- p4.cpp
+++ p4.cpp
@@ -12,16 +12,17 @@
 using namespace std;
 
 int read_input(char **argv);
-enum Redirect {PIPE, REDIRECT, NEITHER};
+enum Redirect {PIPE, STDIN ,STDOUTA, STDOUTT, STDERR, NEITHER};
 Redirect parse_input(int argc, char** argv, char** cmd1, char** cmd2);
 void pipe_cmd(char**, char**);
 void run_cmd(int argc, char** argv);
 void handle_sig(int sig);
-void redirect_cmd(char** cmd, char** file);
-void close_pipe(int fds[2]);
+void redirect_stdoutt(char** cmd, char** file);
+void redirect_stdouta(char** cmd, char** file);
+void close_pipe(int fds [2]);
 
 int main () {
-
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
   char *argv[256], *cmd1[256], *cmd2[256], cwd[1024];
   int argc;
   Redirect pipe_redirect;
@@ -41,8 +42,10 @@ int main () {
 
     if (pipe_redirect == PIPE) {
       pipe_cmd(cmd1, cmd2);
-    } else if (pipe_redirect == REDIRECT) {
-      redirect_cmd(cmd1,cmd2);
+    } else if (pipe_redirect == STDOUTT) {
+      redirect_stdoutt(argv,cmd1);
+    } else if (pipe_redirect == STDOUTA) {
+redirect_stdouta(argv,cmd1);
     } else {
       run_cmd(argc,argv);
     }
@@ -53,7 +56,6 @@ int main () {
 
   return 0;
   }
-
 
 int read_input(char **argv) {
   char *cstr;
@@ -66,16 +68,16 @@ int read_input(char **argv) {
     // convert string into C string
     cstr = new char[arg.size()+1];
     strcpy(cstr, arg.c_str());
- argv[argc] = cstr;
-    // Increase counter of where we're at in the array of argv
+    argv[argc] = cstr;
+    // Increment our counter of where we're at in the array of arguments.
     argc++;
-    // If 'enter' is the input, stop reading input
+    // If the user hit enter, stop reading input.
     if (cin.get() == '\n')
       break;
   }
   // Have to have the last argument be NULL so that execvp works.
   argv[argc] = NULL;
-  // Return the number of arguments 
+  // Return the number of arguments we got.
   return argc;
 }
 
@@ -83,85 +85,88 @@ Redirect parse_input(int argc, char** argv, char** cmd1, char** cmd2) {
   Redirect result = NEITHER;
 
   int split = -1;
-
-  for (int i = 0; i < argc; i++) {
+ for (int i = 0; i < argc; i++) {
     if (strcmp(argv[i],"|") == 0) {
-        result = PIPE;
-        split = i;
+      result = PIPE;
+      split = i;
       } else if (strcmp(argv[i], ">>") == 0) {
-        result = REDIRECT;
-        split = i;
-		
-		// Exit builtin
+      result = STDOUTA;
+      split = i;
+    }  else if (strcmp(argv[i],">") == 0) {
+      result = STDOUTT;
+      split = i;
+    } else if (strcmp(argv[i],"<") == 0) {
+      result = STDIN;
+      split = i;
+    } else if (strcmp(argv[i],"e>") == 0){
+      result = STDERR;
+      split = i;
     } else if (strcmp(argv[i],"exit") == 0) {
       exit(0);
-	  
-	  // CD builtin
     } else if (strcmp(argv[i],"cd") == 0) {
       cout << "cd to " << argv[i+1] << endl;
       chdir(argv[i+1]);
-	  
-	  // Help builtin.
     } else if (strcmp(argv[i],"help") == 0) {
       cout << "builtins avaliable: " << endl;
       cout << "cd [PATH] - Change the current directory to PATH." << endl;
       cout << "exit [N] - Cause the shell to exit with a status of N. N can be omitted." << endl;
       cout << "help - Display helpful information about builtin commands." << endl;
-    }
-  }
-  if (result != NEITHER) {
+    } else if (strcmp(argv[i],"export") == 0) {
+      cout << "export plz" << endl;
+}
+}
+ if (result != NEITHER) {
     // Go through the array of arguments up to the point where the
-    // pipe/redirect was found and store each of those arguments in cmd1 
+    // pipe/redirect was found and store each of those arguments in cmd1.
     for (int i=0; i<split; i++)
       cmd1[i] = argv[i];
 
     // Go through the array of arguments from the point where the pipe/redirect
     // was found through the end of the array of arguments and store each
-    // argument in cmd2 to use for executing pipes/redirects
+    // argument in cmd2.
     int count = 0;
     for (int i=split+1; i<argc; i++) {
       cmd2[count] = argv[i];
       count++;
     }
 
-    // Terminate cmd1 and cmd2 with NULL, so that execvp works
+    // Terminate cmd1 and cmd2 with NULL, so that execvp likes them.
     cmd1[split] = NULL;
     cmd2[count] = NULL;
   }
 
-  // Return an enum showing whether a pipe, redirect, or neither was found
+  // Return an enum showing whether a pipe, redirect, or neither was found.
   return result;
 }
 
 void run_cmd(int argc, char** argv) {
-  pid_t pid, pgid;
+  pid_t pid, pgid = getpgrp();
   int pstatus;
   const char *amp;
   amp = "&";
   bool found_amp = false;
+
 
   if (strcmp(argv[argc-1],amp) == 0)
     found_amp = true;
 
   if ((pid = fork()) == -1)  // error
     perror("FORK ERROR");
-
-   if (pid == 0) { // child
+ if (pid == 0) { // child
+     pid = getpid();
+     if (pgid == 0) pgid = pid;
+     setpgid(pid,pgid);
+     cout << pid << " " << pgid << endl;
     if (found_amp) {
       argv[argc-1] = NULL;
       argc--;
     }
-    setpgid(pid,pgid);
     execvp(argv[0],argv);
     perror("execvp");
    } // end child
-   else { // parent
-   waitpid(pid,NULL,0);
-   setpgid(pid,pgid);
-   cout << "pid: " << pid << endl;
-   }
+   else if (!found_amp)  // parent
+     waitpid(pid,NULL,0);
 }
-
 
 void handle_sig(int sig) {
   cout << "hello" << endl;
@@ -176,87 +181,94 @@ void handle_sig(int sig) {
   }
 }
 
-void redirect_cmd(char** cmd, char** file) {
+void redirect_stdoutt(char** cmd, char** file) {
   int fds[2]; // file descriptors
   int count;  // used for reading from stdout
   int fd;     // single file descriptor
   char c;     // used for writing and reading a character at a time
   pid_t pid;  // will hold process ID; used with fork()
-
-  pipe(fds);
-
   // child process #1
   if (fork() == 0) {
-
-    fd = open(file[0], O_RDWR | O_CREAT, 0666);
-
+    fd = open(file[1], O_RDWR | O_CREAT | O_TRUNC, 0666);
     // open() returns a -1 if an error occurred
     if (fd < 0) {
       printf("Error: %s\n", strerror(errno));
       return;
     }
 
-    dup2(fds[0], 0);
-
-    // Don't need stdout end of pipe.
-    close(fds[1]);
-
-    // Read from stdout...
+    dup2(fd, 1);
+ // Read from stdout...
     while ((count = read(0, &c, 1)) > 0)
       write(fd, &c, 1); // Write to file.
 
-    execlp("echo", "echo", NULL);
- // child process #2
-  } else if ((pid = fork()) == 0) {
-    dup2(fds[1], 1);
-
-    // Don't need stdin end of pipe
-    close(fds[0]);
-
-    // Output contents of the given file to stdout
-    execvp(cmd[0], cmd);
-    perror("execvp failed");
-
-    // parent process
+    execvp(cmd[1],cmd);
   } else {
     waitpid(pid, NULL, 0);
   }
 }
 
+void redirect_stdouta(char** cmd, char** file) {
+  int fds[2]; // file descriptors
+  int count;  // used for reading from stdout
+  int fd;     // single file descriptor
+  char c;     // used for writing and reading a character at a time
+  pid_t pid;  // will hold process ID; used with fork()
 
+
+  // child process #1
+  if (fork() == 0) {
+
+      fd = open(file[2], O_RDWR |O_APPEND | O_CREAT, 0666);
+
+      // open() returns a -1 if an error occurred
+      if (fd < 0) {
+        printf("Error: %s\n", strerror(errno));
+        return;
+      }
+
+      dup2(fd, 1);
+
+      // Read from stdout...
+      while ((count = read(0, &c, 1)) > 0)
+        write(fd, &c, 1); // Write to file.
+
+    } else {
+      waitpid(pid, NULL, 0);
+    }
+}
 void pipe_cmd(char** cmd1, char** cmd2) {
   int fds[2]; // file descriptors
   pipe(fds);
-  pid_t pid;
-  if (fork() == 0) {
+  pid_t pid1, pid2;
+  // child process #1
+  if (pid1 = fork() == 0) {
     // Reassign stdin to fds[0] end of pipe
     dup2(fds[0], 0);
-
     // Not going to write in this child process, so we can close this end
     // of the pipe
-    close_pipe(fds);
-
+    close(fds[1]);
     // Execute the second command
-    execvp(cmd2[0], cmd2);
+    execvp(cmd1[0], cmd1);
     perror("execvp failed");
 
     // child process #2
-  } else if ((pid = fork()) == 0) {
+  } else if ((pid2 = fork()) == 0) {
     // Reassign stdout to fds[1] end of pipe
     dup2(fds[1], 1);
 
     // Not going to read in this child process, so we can close this end
     // of the pipe
-    close_pipe(fds);
+    close(fds[1]);
 
     // Execute the first command
-    execvp(cmd1[0], cmd1);
+    execvp(cmd2[0], cmd2);
     perror("execvp failed");
 
     // parent process
   } else {
-    close_pipe(fds);
-    waitpid(pid, NULL, 0);
+    close(fds[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2,NULL,0);
   }
 }
 
